6db31582640bfbade60260948a0201b44ebbe167
Encelad <gorba4evkirill@gmail.com>
2021-07-10 12:49:43 +0300

    Добавьте файлы проекта.

---
+++ Alarm_Clock_ServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alarm_Clock_Service
+{
+    [RunInstaller(true)]
+    public partial class Alarm_Clock_ServiceInstaller : System.Configuration.Install.Installer
+    {
+        public Alarm_Clock_ServiceInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
